a940cd41fe50246750853b62b25a56e382778697
vassiliymarkov <88937545+vassiliymarkov@users.noreply.github.com>
2024-07-07 12:36:14 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     console.log(`Animating element ${index + 1}`);
                     element.style.opacity = 1;
                     element.style.transform = 'translateY(0)';
-                }, index * 1000); // Задержка 100 мс между элементами
+                }, index * 300); // Задержка 100 мс между элементами
             });
         }, 1000); // Задержка 2.5 секунд
    1 }
